e7d30f1896c7520d0f902289a07695303f7c8fad
はなくそ <84660842+Hanakuso2525@users.noreply.github.com>
2021-12-11 17:46:26 +0900

    公開開始：v.1.0.0

---
+++ index.py
@@ -0,0 +1,55 @@
+try:
+    import json
+    import re
+    from discord.ext import commands
+    import discord, aiohttp
+except ModuleNotFoundError:
+    print("install.bat実行しました？？\n実行してないならしてくださいね！")
+    exit()
+
+with open("config.json") as f:
+    config = json.load(f)
+
+bot = commands.Bot(
+    command_prefix=config["prefix"],
+    help_command=None,
+    allowed_mentions=discord.AllowedMentions(replied_user=False),
+)
+
+R18_color = 0xff66b8
+session = aiohttp.ClientSession()
+
+async def neko_api_image(type: str):
+    async with await session.get("https://nekobot.xyz/api/image?type=%s" % type) as res:
+        res = await res.json()
+    return res.get("message")
+
+@bot.event
+async def on_ready():
+    print("起動完了！")
+
+#おっぱい画像表示
+@bot.command()
+async def boobs(ctx):
+    img = await neko_api_image("boobs")
+    e = discord.Embed(title="R18画像", color=R18_color)
+    e.set_image(url=img)
+    await ctx.reply(embed=e)
+
+#アナル画像表示
+@bot.command()
+async def anal(ctx):
+    img = await neko_api_image("anal")
+    e = discord.Embed(title="R18画像", color=R18_color)
+    e.set_image(url=img)
+    await ctx.reply(embed=e)
+
+########他にもNekoAPIを使えば沢山の画像を取得できます。
+########是非、NekoAPIのドキュメントを見て他にも沢山作ってみてください！！！
+'''
+img = await neko_api_image("anal")
+ここの部分の "anal" のところをドキュメントで確認した画像IDに変更してみてください！
+'''
+
+if __name__ == "__main__":
+    bot.run(config["Token"])
